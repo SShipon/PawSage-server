297b48e3835c031dafe84ecfd092f3d91431d13f
SShipon <nxshipon@gmail.com>
2024-09-30 23:35:43 -0700

    auth user interface add

---
+++ src/app/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import { AuthRoutes } from '../modules/auth/auth.route';
+
+
+const router = Router();
+
+// parent route assign
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+
+ 
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
