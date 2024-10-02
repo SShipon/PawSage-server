fb6d55a8d2e5e194e2e8b0395b6fdbb43832bec2
SShipon <nxshipon@gmail.com>
2024-10-02 12:05:12 -0700

    make the vote working add

--- src/app/modules/vote/vote.validation.ts
+++ src/app/modules/vote/vote.validation.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
-const LikeValidationSchema = z.object({
+const voteValidationSchema = z.object({
   body: z.object({
     userId: z.string({ required_error: 'User ID is required' }),
     postId: z.string({ required_error: 'Post ID is required' }),
   }),
 });
 
-export const LikeValidation = {
-  LikeValidationSchema,
+export const VoteValidation = {
+  voteValidationSchema,
 };
